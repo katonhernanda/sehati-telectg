0c578bd60923e4ba559eeb50511eb57927905df6
Katon Hernanda <44339509+katonhernanda@users.noreply.github.com>
2020-12-16 17:42:54 +0700

    Update data_update.py

--- data_update.py
+++ data_update.py
@@ -32,45 +32,11 @@ scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/aut
 credentials = ServiceAccountCredentials.from_json_keyfile_name('../../config/Connect-gsheet.json', scope)
 gc = gs.authorize(credentials)
 
-bidan_1_maret = pd.read_sql_query('''
-select 
-	users.full_name, users.phone, users.email, users.date_of_birth,
-	midwives.str_number, midwives.str_expiry_date, midwives.ibi_number, midwives.sipb_number,
-	hg."name" as kind, 
-	healthcares.id as tp_id, healthcares.code as tp_code, healthcares.name as tp_name, healthcares.region_id as tp_region_id,
-	tp_districts.name as tp_district, tp_city.name as tp_city, tp_province.name as tp_province, 
-	steering.id as parent_id, steering.code as parent_code, steering.name as parent_name, steering.region_id,
-	steering_districts.name as steering_district, steering_city.name as steering_city, steering_province.name as steering_province, 
-	works."owner", works."primary", works.deleted_at,
-	case 
-	when works.deleted_at is null
-	  then 'false'
-	else
-	  'true'
-	end as deleted
-from users 
-left join midwives on midwives.user_id = users.id 
-left join works on works.user_id = users.id 
-left join healthcares on healthcares.id = works.healthcare_id 
-left join healthcare_groups hg on hg.id = healthcares.healthcare_group_id
-left join regions tp_districts on tp_districts.id = healthcares.region_id 
-left join regions tp_city on tp_city.id = tp_districts.parent_id 
-left join regions tp_province on tp_province.id = tp_city.parent_id 
-left join healthcares steering on steering.id = healthcares.parent_id 
-left join regions steering_districts on steering_districts.id = steering.region_id 
-left join regions steering_city on steering_city.id = steering_districts.parent_id 
-left join regions steering_province on steering_province.id = steering_city.parent_id 
-where role = 'midwife'
-and (
-	(users.created_at::date >= current_date - 1 or users.updated_at::date >= current_date - 1) 
-	or (healthcares.created_at::date >= current_date - 1 or healthcares.updated_at::date >= current_date - 1) 
-	or (works.created_at::date >= current_date - 1 or works.updated_at::date >= current_date - 1)
-)
-order by users.created_at desc 
+query = pd.read_sql_query('''
 ''',cnx)
 
-ws = gc.open("Data Bidan Verifikasi").worksheet("Bidan  > 1 Maret")
+ws = gc.open(" ").worksheet(" ")
 dataframe = pd.DataFrame(ws.get_all_records())
 length = (len(dataframe.index) + 2)
-gd.set_with_dataframe(ws, bidan_1_maret,row = length ) 
+gd.set_with_dataframe(ws, query,row = length ) 
 ws.delete_row(length)
